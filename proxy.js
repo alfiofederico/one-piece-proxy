1d850e5a9b2bc79052d3587742c114889bc40080
vtrofin <victor_trofin@yahoo.com>
2020-07-09 01:35:49 +0900

    move proxy to separate repo

---
+++ proxy.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const request = require('request');
+const baseApiUrl = 'https://onepiececover.com';
+
+const getDataFromApi = (requestUrl, res) => {
+  return request({ url: requestUrl }, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      return res.status(500).send({
+        type: 'error',
+        message: error.message,
+        stack: error.stack,
+      });
+    } else {
+      return res.status(200).send(body);
+    }
+  });
+};
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/api/chapters/', (req, res) => {
+  const urlPath = req.path;
+  const requestUrl = `${baseApiUrl}${urlPath}`;
+  return getDataFromApi(requestUrl, res);
+});
+
+app.get('/api/chapters/:number', (req, res) => {
+  const urlPath = req.path;
+  const requestUrl = `${baseApiUrl}${urlPath}`;
+  return getDataFromApi(requestUrl, res);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
